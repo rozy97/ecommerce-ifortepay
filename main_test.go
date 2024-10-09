7f1b8916f7cd46a63a781f0f44862d48fc985e12
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2024-10-10 05:14:14 +0700

    add failed test case example

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_(t *testing.T) {
+	t.Run("run example test", func(t *testing.T) {
+		num := 2 + 2
+		assert.Equal(t, 5, num)
+	})
+
+}
